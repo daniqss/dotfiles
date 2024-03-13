c4303d86e153d1b9c6ded6609bbac3abbefc8e90
ranicocs <danielqueijo14@gmail.com>
2024-03-13 18:33:14 +0100

    Initial commit for new dotfiles

--- scripts/intellij.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-directory="$1"
-
-/opt/idea-IU-232.9559.62/bin/idea.sh > /dev/null 2>&1 $directory & disown
--- scripts/setup/test.js
+++
@@ -1,5 +0,0 @@
-const l = 34
-
-const str = "test ${l}"
-
-console.log(`${str}`)
