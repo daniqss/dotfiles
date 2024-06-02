ffa3fcd9f4f8fbc114b560e7ee0c6b85f30eb487
ranicocs <danielqueijo14@gmail.com>
2024-06-02 19:16:46 +0200

    Change install script

--- install/install_packages.sh
+++ install/install_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 main() {
-    echo "This script install most of the packeges I'm currently using"
+    echo "This script installs most of the packages I'm currently using"
     echo "It's meant to be run on Arch-based systems after a minimal installation in a tty session"
     echo "Please make sure you have git and yay installed before running this script"
 
@@ -12,10 +12,11 @@ main() {
     fi
 
     echo "Updating system..."
-    sudo pacman -Syu
+    sudo pacman -Syu --noconfirm
+
     # Install packages
     install_pacman_packages
-    # install_yay_packages
+    install_yay_packages
 
     read -p "Do you want to install Nvidia packages? (y/n): " nvidia
     nvidia="${nvidia,,}"
@@ -29,130 +30,122 @@ main() {
 # Function to install packages using pacman
 install_pacman_packages() {
     echo "Installing pacman packages..."
-    for package in "${pacman_packages[@]}"; do
-        sudo pacman -S --noconfirm "$package"
-    done
+    sudo pacman -S --noconfirm "${pacman_packages[@]}"
 }
 
 # Function to install packages using yay
 install_yay_packages() {
     echo "Installing yay packages..."
-    for package in "${yay_package[@]}"; do
-        yay -S --noconfirm ${yay_packages[@]}
-    done
+    yay -S --noconfirm "${yay_packages[@]}"
 }
 
+# Function to install Nvidia packages
 install_nvidia_packages() {
     echo "Installing Nvidia packages..."
-    for package in "${nvidia_packages[@]}"; do
-        sudo pacman -S --noconfirm ${nvidia_packages[@]}
-    done
+    sudo pacman -S --noconfirm "${nvidia_packages[@]}"
 }
 
 # Define pacman packages
 pacman_packages=(
     # Essentials
-    "base-devel"
-    "linux-headers"
-    "linux-firmware"
-    "pacman-contrib"
-    "meson"
-    "ninja"
-    "cmake"
+    base-devel
+    linux-headers
+    linux-firmware
+    pacman-contrib
+    meson
+    ninja
+    cmake
     
     # Hyprland
-    "hyprland"
-    "xdg-desktop-portal-hyprland"
-    "hyprpaper"
-    "hyprpicker"
-    "hyprcursor"
-    "hyprshot"
-    "hyprlock"
-    "hypridle"
+    hyprland
+    xdg-desktop-portal-hyprland
+    hyprpaper
+    hyprpicker
+    hyprcursor
+    hyprshot
+    hyprlock
+    hypridle
     
     # Terminal
-    "kitty"
-    "zsh"
-    "starship"
-    "neovim"
-    "micro"
-    "fastfetch"
-    "htop"
-    "bottom"
-    "exa"
-    "bat"
+    kitty
+    zsh
+    starship
+    neovim
+    micro
+    fastfetch
+    htop
+    bottom
+    exa
+    bat
     
     # Audio
-    "pavucontrol"
-    "pipewire"
-    "gst-plugin-pipewire"
-    "pipewire"
-    "pipewire-alsa"
-    "pipewire-audio"
-    "pipewire-jack"
-    "pipewire-pulse"
-    "wireplumber"
+    pavucontrol
+    pipewire
+    gst-plugin-pipewire
+    pipewire-alsa
+    pipewire-audio
+    pipewire-jack
+    pipewire-pulse
+    wireplumber
     
     # Network
-    "networkmanager"
-    "nm-connection-editor"
-    "bluez"
-    "bluez-utils"
-    "blueberry"
+    networkmanager
+    nm-connection-editor
+    bluez
+    bluez-utils
+    blueberry
     
     # Apps
-    "wofi"
-    "dunst"
-    "waybar"
-    "obsidian"
-    "thunar"
-    "steam"
-    "discord"
-    "chromium"
+    chromium
+    wofi
+    dunst
+    waybar
+    obsidian
+    thunar
+    steam
+    discord
     
     # Development
-    "nodejs"
-    "npm"
-    "bun"
-    "python"
-    "python-pip"
-    "python-pipx"
-    "rust"
+    nodejs
+    npm
+    bun
+    python
+    python-pip
+    python-pipx
+    rust
 )
 
 # Define yay packages
 yay_packages=(
     # Apps
-    # "visual-studio-code-bin"
-    # "brave-bin"
-    "spotify"
-    "xpadneo"
-    # "google-chrome-stable"
+    spotify
+    xpadneo
 
     # Fonts
-    "ttf-cascadia-code"
-    "ttf-cascadia-code-nerd"
-    "ttf-firacode-nerd"
-    "ttf-jetbrains-mono"
-    "ttf-liberation"
-    "ttf-nerd-fonts-symbols"
-    "ttf-opensans"
-    "ttf-roboto"
-    "noto-fonts-emoji",
-    "ttf-ubuntu-nerd"
-    "ttf-nerd-fonts-symbols-common"
+    ttf-bitstream-vera
+    ttf-cascadia-code
+    ttf-dejavu
+    ttf-cascadia-code-nerd
+    ttf-firacode-nerd
+    ttf-jetbrains-mono
+    ttf-liberation
+    ttf-nerd-fonts-symbols
+    ttf-nerd-fonts-symbols-common
+    ttf-opensans
+    ttf-roboto
+    noto-fonts-emoji
+    ttf-ubuntu-nerd
 )
 
 # Define Nvidia packages
 nvidia_packages=(
-    "lib32-nvidia-utils"
-    "nvidia"
-    "nvidia-hook"
-    "nvidia-inst"
-    "nvidia-settings"
-    "nvidia-utils"
-    "opencl-nvidia"
+    nvidia
+    nvidia-utils
+    nvidia-dkms
+    egl-wayland
+    lib32-nvidia-utils
+    nvidia-settings
+    opencl-nvidia
 )
 
 main
-
