57fee42d1ed16d209f2213b5377ba7df3f8843d3
daniqss <danielqueijo14@gmail.com>
2024-08-28 14:27:15 +0200

    add rofi powermenu and wallpaper selector

---
+++ .config/rofi/powermenu.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+## Author : Aditya Shakya (adi1090x)
+## Github : @adi1090x
+#
+## Rofi   : Power Menu
+#
+## Available Styles
+#
+## style-1   style-2   style-3   style-4   style-5
+
+# Current Theme
+dir="$HOME/.config/rofi/"
+theme='config'
+
+# CMDs
+uptime="`uptime -p | sed -e 's/up //g'`"
+host=`hostname`
+
+# Options
+shutdown='  Shutdown'
+reboot='  Reboot'
+lock='  Lock'
+suspend='󰒲  Suspend'
+logout='󰩈  Logout'
+
+# Rofi CMD
+rofi_cmd() {
+	rofi -dmenu \
+		-p "$host" \
+		-mesg "Uptime: $uptime" \
+		-theme ${dir}/${theme}.rasi
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $shutdown)
+		poweroff
+        ;;
+    $reboot)
+        systemctl reboot
+        ;;
+    $lock)
+        hyprlock
+        ;;
+    $suspend)
+        mpc -q pause
+        amixer set Master mute
+        systemctl suspend
+        ;;
+    $logout)
+		hyprctl dispatch exit
+        ;;
+esac
